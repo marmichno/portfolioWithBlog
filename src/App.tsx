7d495a4556aabee3b2393e444adec53f8270c63d
Marcin Michno <mmichno179@gmail.com>
2022-03-14 21:16:06 +0100

    default-routes-blogpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './App.css';
+// config
+import { paths } from './config/paths';
 // components
 import { LandingPage } from '../src/routes/landingPage/LandingPage';
+import { BlogPage } from './routes/blogPage/BlogPage';
+// default
+import { PageNotFound } from './routes/defaultRoutes/pageNotFound/PageNotFound';
+// router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path={paths.home} element={<LandingPage />} />
+        <Route path={paths.blog} element={<BlogPage />} />
+        <Route path={paths.notFound} element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
